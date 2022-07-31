8a6a677a76cef4c1fbca35adee6da022c069e5dc
Bill Rossi <bassguitarbill@gmail.com>
2022-07-31 04:46:21 -0400

    Try fixing email protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   host = 'intense-plains-70311.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => host }
+  config.action_mailer.default_url_options = { :host => host, protocol: "https" }
 
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
