b8a9a712dfbcc5e4074904a1d46cd7b7676eac31
Bill Rossi <bassguitarbill@gmail.com>
2022-07-31 07:31:56 -0400

    Use proper rails methods

--- app/controllers/locale_controller.rb
+++ app/controllers/locale_controller.rb
@@ -1,7 +1,6 @@
 class LocaleController < ApplicationController
   def set_locale
-    current_user.locale = params[:locale]
-    current_user.save
+    current_user.update(locale: params[:locale])
     redirect_to root_path
   end
 end
