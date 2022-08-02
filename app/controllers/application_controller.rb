abf5d2bccab0620a56ed1798ac41a376a27af1ea
Bill Rossi <bassguitarbill@gmail.com>
2022-08-02 18:35:31 -0400

    Stop unsigned in users from doing anything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   include Passwordless::ControllerHelpers # <-- This!
 
   around_action :set_time_zone
+  before_action :require_user!
 
   helper_method :current_user
   helper_method :current_locale
 
   private
+
 
   def current_user
     @current_user ||= authenticate_by_session(User)
@@ -17,8 +19,8 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user!
-    return if current_user
-    redirect_to root_path, flash: { error: 'You are not worthy!' }
+    return if current_user || request.path == auth.sign_in_path || (params[:token] && request.path == auth.token_sign_in_path)
+    redirect_to auth.sign_in_path, flash: { error: 'You are not worthy!' }
   end
 
   def set_time_zone(&block)
