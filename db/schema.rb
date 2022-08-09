98ed2731dee20e7bd9cb874ecb99d126f76e1d60
Bill Rossi <bassguitarbill@gmail.com>
2022-08-08 20:35:57 -0400

    Add column 'discord' to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_222939) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_003135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_222939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "locale"
+    t.string "discord"
   end
 
   add_foreign_key "prompt_grammars", "users", column: "added_by_id"
