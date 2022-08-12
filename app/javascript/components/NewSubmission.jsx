0d014ed09b17be36d53d81c8250bd77c8887f262
Bill Rossi <bassguitarbill@gmail.com>
2022-08-12 06:38:14 -0400

    Put some spacing in the New Submission page

--- app/javascript/components/NewSubmission.jsx
+++ app/javascript/components/NewSubmission.jsx
@@ -2,6 +2,10 @@ import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 
+import Button from 'rsuite/Button';
+import Divider from 'rsuite/Divider';
+import Input from 'rsuite/Input';
+
 function generatePrompt({ length, topic, grammar }) {
   return `Write ${length} about ${topic} using ${grammar}`;
 }
@@ -38,22 +42,17 @@ export default function NewSubmission() {
   };
 
   return (
-    <div>
+    <div className="new-submission">
       <If condition={shouldRedirect}>
         <Redirect to="/submissions" />
       </If>
-      <div>
-        <button onClick={fetchPrompt}>Generate Prompt</button>
-      </div>
-      <div>
-        <textarea value={prompt} onChange={handlePromptInput} />
-      </div>
-      <div>
-        <textarea value={response} onChange={handleResponseInput} />
-      </div>
-      <div>
-        <button onClick={submit}>Submit</button>
-      </div>
+      <Button size="sm" onClick={fetchPrompt}>Generate Prompt</Button>
+      <Divider />
+      <Input className="new-submission-input" value={prompt} as="textarea" rows={3} placeholder="Prompt" onChange={handlePromptInput} />
+      <Divider />
+      <Input className="new-submission-input" value={response} as="textarea" rows={3} placeholder="Response" onChange={handleResponseInput} />
+      <Divider />
+      <Button appearance="primary" onClick={submit}>Submit</Button>
     </div>
   );
 }
